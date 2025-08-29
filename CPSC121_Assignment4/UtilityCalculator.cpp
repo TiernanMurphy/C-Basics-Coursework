0a37e66cd0019b8c43c3d694f0110dcc29db2061
Tiernan Murphy <156395514+TiernanMurphy@users.noreply.github.com>
2025-08-28 18:04:02 -0700

    utility expense calculator
    early CS1 program using conditionals to calculate utility bill

---
+++ CPSC121_Assignment4/UtilityCalculator.cpp
@@ -0,0 +1,28 @@
+/**
+ * file:            UtilityCalculator.cpp
+ * author:          <your name>
+ * date modified:   <last date you modified code>
+ * description:     <short description of what the program does>
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    char customerCode;
+    int meterStart, meterEnd;
+
+    string bannerMessage;
+    bannerMessage += "+-----------------------+\n";
+    bannerMessage += "|  Utility  Calculator  |\n";
+    bannerMessage += "+-----------------------+\n";
+    cout << bannerMessage << endl;
+
+    // You finish ...
+    
+    
+
+    return 0;
+}
